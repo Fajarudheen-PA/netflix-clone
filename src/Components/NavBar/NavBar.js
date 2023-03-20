6d4779091a37196abbcf39caff0db70991a0e7c7
Fajarudheen-PA <fajarudheen98@gmail.com>
2023-03-20 16:27:01 +0530

    updated NavBar and ProfDrawer

--- src/Components/NavBar/NavBar.js
+++ src/Components/NavBar/NavBar.js
@@ -3,10 +3,15 @@ import "./NavBar.css";
 // import ProfDrawer from '../ProfDrawer/ProfDrawer';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faUser, faHeart } from "@fortawesome/free-regular-svg-icons";
-import { faGear, faHeadset } from "@fortawesome/free-solid-svg-icons";
+import { faClose, faGear, faHeadset } from "@fortawesome/free-solid-svg-icons";
 
 function NavBar() {
   const [prof,setProf] = useState(false)
+
+  const closeProfDrawer = () => {
+    setProf(false);
+  };
+  
   return (
     <div className='navbar'>
       <div className='content-wrapper'>
@@ -15,17 +20,24 @@ function NavBar() {
           <img onClick={()=>setProf(!prof)} className='avatar' src="https://i.pinimg.com/originals/0d/dc/ca/0ddccae723d85a703b798a5e682c23c1.png" alt="Avatar" />
         </div>
       </div>  
-        { prof && <ProfDrawer/> }
+        { prof && <ProfDrawer onClose={closeProfDrawer}/> }
     </div>
   )
 }
 
+// function closeClick(){
+//   const element = document.getElementById('closeIcon');
+//   element.classList.add('closing');
+// }
 
-function ProfDrawer() {
+
+function ProfDrawer(props) {
+  const { onClose } = props;
   return (
     <div className='content-wrapper'>
       <div className='profile-drawer'>
         <div>
+            <div className='closing'><FontAwesomeIcon icon={faClose} onClick={onClose} className='close'/></div>
             <li title='My Account'><FontAwesomeIcon icon={faUser}/> My Account</li>
             <li title='Watchlist'><FontAwesomeIcon icon={faHeart}/> Watchlist</li>
             <li title='Settings'><FontAwesomeIcon icon={faGear}/> Settings</li>
